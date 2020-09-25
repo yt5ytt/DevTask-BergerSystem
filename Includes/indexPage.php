0c2eefd598c3bc0705106638778f24ca67ac56fc
yt5ytt <yt5ytt@gmail.com>
2020-09-25 12:52:24 +0200

    ...

---
+++ Includes/indexPage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Berger system</title>
+    <link rel="stylesheet" href="<?php echo URL; ?>Includes/css/master.css">
+  </head>
+  <body>
+    <div class="main">
+      <div class="title">
+        <h1>BERGER SYSTEM</h1>
+      </div>
+      <div class="home">
+        This is Home div
+      </div>
+      <div class="coma">
+        This is coma div
+      </div>
+      <div class="away">
+        This is Away div
+      </div>
+    </div>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -10,10 +10,17 @@
   define('ABSPATH', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 
   /**
-   * Include Composer autoload file
+   * Define absolute url to the bergerSystem folder
    */
+   define('URL' , $_SERVER['HTTP_REFERER'] . basename(__DIR__) . DIRECTORY_SEPARATOR);
+
+  /**
+   * Include Composer autoload file
+   */   
   include(ABSPATH . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 
   use App\ParticipansTable;
 
   new ParticipansTable();
+
+  include(ABSPATH . 'Includes' . DIRECTORY_SEPARATOR . 'indexPage.php');
