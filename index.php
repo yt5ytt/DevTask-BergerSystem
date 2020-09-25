d528f0b1363166dd681483d0b8853bfc33316547
yt5ytt <yt5ytt@gmail.com>
2020-09-25 09:51:50 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+  /**
+   * @package Berger system
+   */
+
+  /**
+   * Define absolute path to the bergerSystem folder
+   */
+  define('ABSPATH', dirname(__FILE__) . DIRECTORY_SEPARATOR);
+
+  /**
+   * Include Composer autoload file
+   */
+  include(ABSPATH . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
+
+  
